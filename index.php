f6170d856c3972e9fe45cac53999cbda9a4d68f2
Charles Gabriel Lemes <charleslemes21@gmail.com>
2021-12-31 17:31:54 -0300

    novo

---
+++ index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0", maximum-scale=1.0>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <title>JL viagens</title>
+</head>
+<body>
+
+    <header>
+    <div class="logo"></div>
+   <div class="container-header">
+      
+        
+        <nav class="menu-desktop">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Lugares</a></li>
+                <li><a href="">Viagens</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+
+      <!-- mobile-->
+      <div class="clear"></div>
+       
+         <nav class="menu-mobile">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Lugares</a></li>
+                <li><a href="">Viagens</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>  
+     
+     </div><!--container-header-->
+        <div class="clear"></div><!--clear-->
+        
+    </header>
+
+    <section>
+        <div class="center">
+            <h2> Lugares </h2>
+            <div class="sublinhado-title"></div><!--sublinado-title-->
+        </div><!--center-->
+
+            <div class="container-slider">
+               
+                    <div class="slider-wraper">
+                      
+                        <div class="img-slider-single"></div>
+                        
+                    </div><!--slider-wraper-->
+
+                    <div class="slider-wraper">
+                        
+                        <div class="img-slider-single"></div>
+                       
+                    </div><!--slider-wraper-->
+
+                    <div class="slider-wraper">
+                       
+                        <div class="img-slider-single"></div>
+                       
+                    </div><!--slider-wraper-->
+                    
+                    <div class="clear"></div>
+        
+                    <div class="slider-bullets">
+                        <span style="background-color:rgb(120,120,120);"></span>
+                        <span></span>
+                        <span></span>
+                    </div><!--slider-bullets-->
+
+            </div><!--container-slider-->
+
+            <div class="clear"></div>
+
+            <div class="container-slider">
+               
+               <div class="slider-wraper">
+                  
+                   <div class="img-slider-single"></div>
+                  
+               </div><!--slider-wraper-->
+
+               <div class="slider-wraper">
+                  
+                   <div class="img-slider-single"></div>
+                   
+               </div><!--slider-wraper-->
+
+               <div class="slider-wraper">
+                   
+                   <div class="img-slider-single"></div>
+                   
+               </div><!--slider-wraper-->
+
+               <div class="slider-bullets">
+                        <span style="background-color:rgb(120,120,120);"></span>
+                        <span></span>
+                        <span></span>
+                    </div><!--slider-bullets-->
+
+       </div><!--container-slider-->
+       
+       <div class="clear"></div>
+    </section>
+
+
+
+
+    <section class="viagens">
+            <div class="center">
+                <h2>Pacotes de viagens</h2>
+                <div class="sublinhado-title2"></div>
+                        <div class="container-viagens">
+
+                            <div class="viagens-wraper">
+                                <h2>Viagem</h2>
+                                <div class="sublinhado-title-viagem"></div>
+                                <div class="img-pacotes-single"></div>
+                                
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa possimus delectus laborum iusto neque rerum reprehenderit.
+                                    
+                                </p>
+                                <div class="btn-viagens">
+                                <a href="">Mais Detalhes</a>
+                                </div><!--btn-viagens-->
+                            </div><!--viagens-wraper-->
+                                <div class="clear"></div>
+                            <div class="viagens-wraper">
+                                <h2>Viagem</h2>
+                                <div class="ksublinhado-title-viagem"></div>
+                                <div class="img-pacotes-single"></div>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa possimus delectus laborum iusto neque rerum reprehenderit.</p>
+                                <div class="btn-viagens">
+                                <a href="">Mais Detalhes</a>
+                                </div><!--btn-viagens-->
+                            </div><!--viagens-wraper-->
+
+                            
+
+       </div><!--container-slider-->
+                    <div style="margin-top: 15px; background-color:transparent;" class="slider-bullets">
+                        <span style="background-color:rgb(120,120,120);"></span>
+                        <span></span>
+                        <span></span>
+                    </div><!--slider-bullets-->
+                        </div><!--container-viagens-->
+                        <div class="clear"></div>
+            </div><!--center-->
+    </section><!--section viagens-->
+
+
+    <section class="contato">
+            <div class="center">
+                <h2>Contato</h2>
+                <div class="sublinado-title-contato"></div><!--sublinado-title-contato-->
+
+                
+                <form id="formulario" >
+                 <div class="formulario-wraper w100">
+                    <input type="text" name="nome" placeholder="Nome*...">
+                 </div><!--formulario-wraper w100-->
+
+                 <div class="formulario-wraper w50">
+                    <input type="text" name="email" placeholder="E-mail*...">
+                 </div><!--formulario-wraper w100-->
+
+                 <div class="formulario-wraper w50">
+                    <input type="text" name="telefone" placeholder="Telefone*... (99)99999-9999">
+                 </div><!--formulario-wraper w100-->
+
+                 <div class="formulario-wraper w100">
+                   <textarea name="mensagem" placeholder="Mensagem*..."></textarea>
+                 </div><!--formulario-wraper w100-->
+
+               <div class="formulario-wraper w100">
+               <input type="submit" name="acao" value="Enviar">
+               </div><!--formulario-wraper-->
+                </form><!--form-->
+                </div><!--formulario-wraper-->
+
+            <div class="clear"></div><!--clear-->
+            </div><!--center-->
+    </section><!--contato-->
+
+
+    <footer>
+        <div class="footer-wraper">
+            <div class="informacao1">
+                <p>Todos seus direitos reservados</p>
+            </div><!--informacao1-->
+  
+            <div class="informacao2">
+                <p>Desenvolvido por <a href="#">Charles Lemes</a></p>
+            </div><!--informacao2-->
+
+        </div><!--footer-wraper-->
+        <div class="clear"></div>
+    </footer>
+
+    
+
+
+
+<script src="js/jquery.js"></script>
+<script src="js/efeitos.js"></script>
+</body>
+</html>
---
+++ js/efeitos.js
@@ -0,0 +1,128 @@
+
+
+$(function(){
+
+
+	/*fazendo script do menu mobile*/ 
+	abrirMenu();
+	function abrirMenu(){
+		var botaoMobile = $('nav.menu-mobile');
+		var slideBotaoMenu = $('nav.menu-mobile ul');
+		botaoMobile.click(function(e){
+			slideBotaoMenu.slideToggle();
+		})
+	}
+
+	/*finalizando mobile*/ 
+
+
+
+	function formularioEnviadoGreen(sucesso){
+		sucesso.css('border','2px solid green');
+	
+
+		
+	}
+
+
+	function campoInvalido(invalido){
+		invalido.css('border','2px solid red');
+		invalido.css('color','red');
+		invalido.val('campo Inválido');
+	}
+
+	function resetInvalido(invalido){
+		invalido.css('border','1.8px solid #db2b2b');
+		invalido.css('color','black');
+		invalido.val('');
+	}
+
+	$('input[type=text], textarea').focus(function(){
+		resetInvalido($(this));
+	});
+
+
+	$('form#formulario').submit(function(e){
+		e.preventDefault();
+		var nome = $('input[name=nome]').val();
+		var email = $('input[name=email]').val();
+		var telefone = $('input[name=telefone]').val();
+		var mensagem = $('textarea').val();
+
+
+		if(validadoNome(nome) == false){
+			campoInvalido($('input[name=nome]'));
+		}
+		else if(validadoEmail(email) == false){
+			campoInvalido($('input[name=email]'));
+		}
+
+		else if(validadoTelefone(telefone) == false){
+			campoInvalido($('input[name=telefone]'));
+		}
+
+		else if(validadoMensagem(mensagem) == false){
+			campoInvalido($('textarea'));
+		}
+
+		else{
+			formularioEnviadoGreen($('input[type=text]'));
+			formularioEnviadoGreen($('textarea'));
+
+
+			$(window).scroll(function(){
+				$('input[type=text]').css('border','1.8px solid #db2b2b');
+				$('textarea').css('border','1.8px solid #db2b2b');
+			})
+
+		}
+
+	})
+
+
+
+	function validadoNome(nome){
+		if(nome == ''){
+			return false;
+		}
+	}
+
+
+	function validadoEmail(email){
+		if(email == ''){
+			return false;
+		}
+
+		if(email.match(/^([a-z0-9-_.]{1,})+@+([a-z.]{1,})$/) == null){
+			return false;
+		}
+
+	}
+
+
+	function validadoTelefone(telefone){
+		if(telefone == ''){
+			return false;
+		}
+
+		if(telefone.match(/^\([0-9]{2}\)[0-9]{5}-[0-9]{4}$/) == null){
+			return false;
+		}
+
+	}
+
+
+	function validadoMensagem(mensagem){
+		if(mensagem == ''){
+			return false;
+		}
+	}
+
+
+
+
+	
+
+	return false;
+
+})
